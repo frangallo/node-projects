245317983fbe93a355f8b5cf207e70429df75e5e
frangallo <fgallo@me.com>
2015-10-20 14:31:17 -0700

    added handles

--- image-upload/requestHandlers.js
+++ image-upload/requestHandlers.js
@@ -1,18 +1,21 @@
-function start(){
+var exec = require("child_process").exec
+
+function start(response){
   console.log("Request handler 'start' was called.")
+  var content = "empty";
 
-  function sleep(milliSeconds){
-    var startTime = new Date().getTime();
-    while (new Date().getTime() < startTime + milliSeconds);
-  }
-
-  sleep(10000)
-  return "Hello Start";
+  exec("ls -lah", function(error, stdout, stderr){
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write(stdout);
+    response.end();
+  });
 }
 
-function upload(){
-  console.log("Request handler 'upload' was called")
-  return "Hello Upload"
+function upload(response){
+  console.log("Request handler 'upload' was called");
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write('Hello Upload');
+  response.end();
 }
 
 exports.start = start;
