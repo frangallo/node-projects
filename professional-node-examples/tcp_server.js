6b7a848e5abe41dbb41bf749cca72c2dec6ddd96
frangallo <fgallo@me.com>
2015-10-22 13:13:35 -0700

    echo tcp server

--- professional-node-examples/tcp_server.js
+++ professional-node-examples/tcp_server.js
@@ -11,25 +11,45 @@
 // }).listen(4001);
 
 
-var server = require('net').createServer();
-var port = 4001;
+// var server = require('net').createServer();
+// var port = 4001;
+//
+// server.on('listening', function(){
+//   console.log('Server is listening on port', port);
+// });
+//
+// server.on('connection', function(socket) {
+//   console.log('Server has a new connection');
+//   socket.end();
+//   server.close();
+// })
+//
+// server.on('close', function(){
+//   console.log('Server is now closed');
+// })
+//
+// server.on('error', function() {
+//   console.log('Error occurred: ', err.message);
+// });
+//
+// server.listen(port)
 
-server.on('listening', function(){
-  console.log('Server is listening on port', port);
-});
+var server = require('net').createServer(function(socket){
+  console.log('new connection');
 
-server.on('connection', function(socket) {
-  console.log('Server has a new connection');
-  socket.end();
-  server.close();
-})
+  socket.setEncoding('utf8');
+  socket.write("Hello! You can start typing. Type 'quit to exit'. \n");
 
-server.on('close', function(){
-  console.log('Server is now closed');
-})
+  socket.on("data", function(data){
+    console.log('got: ', data.toString())
+    if (data.trim().toLowerCase() === 'quit'){
+      socket.write('Bye bye!');
+      return socket.end();
+    }
+    socket.write(data);
+  })
 
-server.on('error', function() {
-  console.log('Error occurred: ', err.message);
-});
-
-server.listen(port)
+  socket.on('end', function(){
+    console.log('Client connection ended');
+  });
+}).listen(4001)
