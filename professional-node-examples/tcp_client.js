3f6ce219e28703d947383a5cbea2d779f296d0a3
frangallo <fgallo@me.com>
2015-10-23 09:39:07 -0700

    added reconnect capabilities

--- professional-node-examples/tcp_client.js
+++ professional-node-examples/tcp_client.js
@@ -2,12 +2,37 @@ var net = require('net');
 var port = 4000;
 var conn;
 
+var retryInterval = 3000;
+var retriedTimes = 0;
+var maxRetries = 10;
+var quitting = false;
+
+
 process.stdin.resume();
 
+process.stdin.on('data', function(data) {
+  if (data.toString().trim().toLowerCase() === 'quit') {
+    quitting = true;
+    console.log("quitting...")
+    conn.end();
+    process.stdin.pause();
+  } else {
+    conn.write(data);
+  }
+});
+
 (function connect(){
+  function reconnet() {
+    if (retiredTimes >= maxRetries) {
+      throw new Error('Max retries have been exceeded');
+    }
+    retiredTimes += 1;
+    setTimeout(connect, retryInterval);
+  }
   conn = net.createConnection(port);
 
   conn.on("connect", function(){
+    retriedTimes = 0
     console.log('connected to server');
   });
 
@@ -16,10 +41,13 @@ process.stdin.resume();
   });
 
   conn.on('close', function() {
-    console.log('connection got closed, will try to reconnet');
-    connect();
+    if (!quitting){
+      console.log('connection got closed, will try to reconnet');
+      reconnect();
+    }
   });
 
   conn.pipe(process.stdout, {end: false});
   process.stdin.pipe(conn);
+
 }());
