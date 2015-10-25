ff7943be8b97758b0c209874ffcd52d29d9b2fff
frangallo <fgallo@me.com>
2015-10-25 15:36:00 -0700

    add user show

--- professional-node-examples/my_app/app.js
+++ professional-node-examples/my_app/app.js
@@ -30,6 +30,7 @@ app.configure('production', function(){
 // Routes
 
 require('./routes/index')(app);
+require('./routes/users')(app)
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
---
+++ professional-node-examples/my_app/routes/users.js
@@ -0,0 +1,5 @@
+module.exports = function(app) {
+  app.get('/users/:name',function(req, res) {
+    res.render('users/profile', {title: 'User profile'});
+  });
+};
