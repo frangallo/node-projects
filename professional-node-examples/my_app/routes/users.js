3af6f24ae4e5606bf7065e5ebf9483950efa60b0
frangallo <fgallo@me.com>
2015-10-25 15:57:18 -0700

    add routes

--- professional-node-examples/my_app/routes/users.js
+++ professional-node-examples/my_app/routes/users.js
@@ -1,5 +1,38 @@
 module.exports = function(app) {
   app.get('/users/:name',function(req, res) {
-    res.render('users/profile', {title: 'User profile'});
+    var user = usres[req.params.name];
+    if(user){
+      res.render('users/profile', {title: 'User profile', user: user});
+    } else {
+      next();
+    }
   });
+
+  app.get('/users/new', function(req, res) {
+    res.render('users/new', {title: "new User"});
+  });
+
+  app.get('/users', function(req, res) {
+    res.render('users/index', {title: 'Users', users: users});
+  });
+
+  app.post('/users', function(req, res) {
+    if (users[req.body.username]) {
+      res.send('Conflict', 409)
+    } else {
+      users[req.body.username] = req.body;
+      res.redirect('/users');
+    }
+  });
+
+  app.del('/users/:name', function(req,res,next){
+    if (users[req.params.name]){
+      delete users[req.params.name];
+      res.redirect('/users');
+    } else {
+      next();
+    }
+  })
+
+
 };
