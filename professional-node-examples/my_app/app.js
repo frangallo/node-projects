597dc10d142e1a3b7c55a6110e16ccee78be8b6d
frangallo <fgallo@me.com>
2015-10-26 06:51:57 -0700

    add session routes

--- professional-node-examples/my_app/app.js
+++ professional-node-examples/my_app/app.js
@@ -37,6 +37,7 @@ app.configure('production', function(){
 
 require('./routes/index')(app);
 require('./routes/users')(app)
+require('./routes/session')(app);
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
