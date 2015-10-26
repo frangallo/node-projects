3a3b66af25941ce5a9b4e88a145970f8c126a0dc
frangallo <fgallo@me.com>
2015-10-26 07:06:46 -0700

    finished added middleware

---
+++ professional-node-examples/my_app/routes/middleware/restrict_user_to_self.js
@@ -0,0 +1,8 @@
+function restrictUserToSelf(req, res, next) {
+  if (! req.session.user || req.session.user.username !== req.user.username) {
+    res.send('Unauthorized', 401); } else {
+      next();
+  }
+}
+
+module.exports = restrictUserToSelf;
--- professional-node-examples/my_app/routes/users.js
+++ professional-node-examples/my_app/routes/users.js
@@ -1,15 +1,17 @@
 var users = require('../data/users');
 var notLoggedIn = require('./middleware/not_logged_in');
+var loadUser = require('./middleware/load_user');
+var restrictUserToSelf = require('./middleware/restrict_user_to_self');
 
 module.exports = function(app) {
-  app.get('/users/new', function(req, res) {
+  app.get('/users/new', notLoggedIn, function(req, res) {
     res.render('users/new', {title: "new User"});
   });
 
-  app.get('/users/:name',function(req, res) {
+  app.get('/users/:name', loadUser, function(req, res) {
     var user = users[req.params.name];
     if(user){
-      res.render('users/profile', {title: 'User profile', user: user});
+      res.render('users/profile', {title: 'User profile', user: req.user});
     } else {
       next();
     }
@@ -19,7 +21,7 @@ module.exports = function(app) {
     res.render('users/index', {title: 'Users', users: users});
   });
 
-  app.post('/users', function(req, res) {
+  app.post('/users', notLoggedIn, function(req, res) {
     if (users[req.body.username]) {
       res.send('Conflict', 409)
     } else {
@@ -28,16 +30,8 @@ module.exports = function(app) {
     }
   });
 
-  app.del('/users/:name', function(req,res,next){
-    if (users[req.params.name]){
-      delete users[req.params.name];
-      res.redirect('/users');
-    } else {
-      next();
-    }
+  app.del('/users/:name', loadUser, restrictUserToSelf, function(req,res,next){
+    delete users[req.params.name];
+    res.redirect('/users');
   });
-
-
-
-
 };
